e837c785999d9331031a980128c666d85759d645
SexyGarbage <sexygarbage@bigmir.net>
2016-11-13 17:45:14 +0300

    Inventory steam commit. Not working edit datagrid WPF.

--- PilotkaBotWPF/Logic/FormsLogic.cs
+++ PilotkaBotWPF/Logic/FormsLogic.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PilotkaBotWPF.JsonClasses;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
@@ -13,8 +15,11 @@ namespace PilotkaBotWPF.Logic
 {
     static class FormsLogic
     {
-       
-
+        private static string average_market_price;
+        private static string max_earnings;
+        private static string highest_market_price;
+        private static string average_earnings;
+        private static string lowest_market_price;
 
         public static void AddColumns(ref DataGridView data, List<string> columnsNames)
         {
@@ -88,5 +93,111 @@ namespace PilotkaBotWPF.Logic
                 }
             }
         }
+
+        internal static void AddColumns(ref System.Windows.Controls.DataGrid data, List<string> columnsNames)
+        {
+            for (int i = 0; i < columnsNames.Count; i++)
+            {
+                DataGridTextColumn textColumn = new DataGridTextColumn();
+                textColumn.Header = columnsNames[i];
+                textColumn.Binding = new System.Windows.Data.Binding(columnsNames[i]);
+                data.Columns.Add(textColumn);
+
+                
+                
+            }
+        }
+
+        internal static void AddSteamPrices(ref System.Windows.Controls.DataGrid data)
+        {
+            for (int i = 0; i < data.Items.Count; i++)
+            {
+                MarketItemInfo marketInfo = Info.GetMarketItemInfo(csgoItemInfo + (data.Items[i] as ItemId).classid + "_"
+                    + (data.Items[i] as ItemId).instanceid + "/ru/?key=" + keyMarket);
+                SteamApiItemCS steamInfo = Info.GetApiItemCS(csgobackpacknet + marketInfo.market_hash_name + "&time=7&icon=1");//Info.GetSteamApiItem(@"http://api.csgo.steamlytics.xyz/v1/prices/" + marketInfo.market_hash_name + "?key=8754a7606d6b00db0cc9ce1dee003256");
+                
+                var newItem = new dataItem
+                {
+                    average_price = steamInfo.average_price,
+                    median_price = steamInfo.median_price,
+                    highest_price = steamInfo.highest_price,
+                    lowest_price = steamInfo.lowest_price,
+                };
+                data.Items[i] = newItem;
+                //data.Rows[i].Cells["hash"].Value = marketInfo.hash;
+                // Thread.Sleep(10500);
+            }
+        }
+
+        public class dataItem
+        {
+            public string average_price;
+            public string median_price;
+            public string highest_price;
+            public string lowest_price;
+        }
+
+        internal static void AddEarnings(ref System.Windows.Controls.DataGrid data)
+        {
+
+            for (int i = 0; i < data.Items.Count; i++)
+            {
+                if (data.Items[i] != null)
+                {/*
+                    data.Rows[i].Cells["min_earnings"].Value = Earnings(data.Rows[i].Cells["lowest_price"].Value.ToString(),
+                        data.Rows[i].Cells["lowest_market_price"].Value.ToString());
+                    data.Rows[i].Cells["average_earnings"].Value = Earnings(data.Rows[i].Cells["average_price"].Value.ToString(),
+                        data.Rows[i].Cells["average_market_price"].Value.ToString());
+                    data.Rows[i].Cells["max_earnings"].Value = Earnings(data.Rows[i].Cells["highest_price"].Value.ToString(),
+                        data.Rows[i].Cells["highest_market_price"].Value.ToString());
+                        */
+                    var newItem = new dataItemEarnings
+                    {
+                        min_earnings = Earnings((data.Items[i] as dataItemEarnings).lowest_price.ToString(),
+                        (data.Items[i] as dataItemEarnings).lowest_market_price.ToString()),
+
+                        lowest_market_price = (data.Items[i] as dataItemEarnings).lowest_market_price.ToString(),
+                        average_earnings = Earnings((data.Items[i] as dataItemEarnings).average_price.ToString(),
+                        (data.Items[i] as dataItemEarnings).average_market_price.ToString()),
+
+                        average_market_price = (data.Items[i] as dataItemEarnings).average_market_price.ToString(),
+                        max_earnings = Earnings((data.Items[i] as dataItemEarnings).highest_price.ToString(),
+                        (data.Items[i] as dataItemEarnings).highest_market_price.ToString()),
+
+                        highest_market_price = (data.Items[i] as dataItemEarnings).highest_market_price.ToString(),
+
+                        lowest_price = (data.Items[i] as dataItemEarnings).lowest_price,
+                        average_price = (data.Items[i] as dataItemEarnings).average_price,
+                        highest_price = (data.Items[i] as dataItemEarnings).highest_price,
+                    };
+                    data.Items[i] = newItem;
+                }
+            }
+        }
+
+        public class dataItemEarnings
+        {
+            public string min_earnings;
+            public string lowest_market_price;
+            public string average_earnings;
+            public string average_market_price;
+            public string max_earnings;
+            public string highest_market_price;
+            public string lowest_price;
+            public string average_price;
+            public string highest_price;
+        }
+
+        public static void SaveItem(ref System.Windows.Controls.DataGrid dataSellSteam, int i, InventorySteamWindow.dataSellSteamItem newItem)
+        {
+            var coll = dataSellSteam.Items.SourceCollection;
+            var outcoll = new List<object>();
+            foreach (var item in coll)
+                outcoll.Add(item);
+            outcoll[i] = newItem;
+            dataSellSteam.ItemsSource = outcoll;
+        }
+
+        
     }
 }
--- PilotkaBotWPF/Windows/InventorySteamWindow.xaml.cs
+++ PilotkaBotWPF/Windows/InventorySteamWindow.xaml.cs
@@ -1,17 +1,24 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using PilotkaBotWPF;
+using PilotkaBotWPF.JsonClasses;
+using PilotkaBotWPF.Logic;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using static PilotkaBotWPF.Logic.API;
 namespace PilotkaBotWPF
 {
     /// <summary>
@@ -22,6 +29,282 @@ namespace PilotkaBotWPF
         public InventorySteamWindow()
         {
             InitializeComponent();
+            //dataGridView1.DataSource = SearchBot.GetUrlJSONInventorySteamlist(@"http://steamcommunity.com/profiles/76561198131830880/inventory/json/570/2/?trading=1");
+            SetDataSourceAllItem();
+            //FormsLogic.AddColumns(ref dataGridViewForSell, new List<string>()
+            //{
+            //    "name",
+            //    "type",
+            //    "min_price",
+            //    "average_market_price",
+            //    "lowest_market_price",
+            //    "highest_market_price",
+            //    "average_price",
+            //    "median_price",
+            //    "highest_price",
+            //    "lowest_price",
+            //    "min_earnings",
+            //    "average_earnings",
+            //    "max_earnings"
+            //});
+            SetDataSourceSellItem(dataGridViewForSell);
+        }
+
+        int rowIndex;
+        string classid, instanceid;
+        int currentColumnIndex = 0;
+        List<JObject> itemsrgInventory;
+        BindingList<JsonInventoryResult> marketItems;
+        BindingList<ItemId> soldSteamItems;
+        
+        public List<ItemId> GetGroupItems(List<ItemId> listItems)
+        {
+            return listItems
+                .GroupBy(itemId => itemId.classid, itemId => itemId.instanceid)
+                .Select(group => new ItemId
+                {
+                    classid = group.Key,
+                    instanceid = group.First(),
+                    count = group.Count()
+                }).ToList();
+        }
+        public BindingList<ItemId> SoldItemsFromSteam(List<ItemId> targetListSteam, List<ItemId> targetListMarket, ref int count)
+        {
+            List<ItemId> tttSteam = GetGroupItems(targetListSteam);
+            List<ItemId> tttMarket = GetGroupItems(targetListMarket);
+            BindingList<ItemId> sellItems = new BindingList<ItemId>();
+            count = 0;
+            if (tttMarket.Count > 0)
+            {
+                foreach (var itttSteam in tttSteam)
+                {
+                    bool found;
+                    foreach (var itttMarket in tttMarket)
+                    {
+                        found = itttSteam.classid == itttMarket.classid &&
+                            itttSteam.instanceid == itttMarket.instanceid;
+                        if (found)
+                        {
+                            itttSteam.count -= itttMarket.count;
+                            count += itttSteam.count;
+                        }
+                    }
+                    if (itttSteam.count != 0)
+                    {
+                        sellItems.Add(itttSteam);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var itttSteam in tttSteam)
+                {
+                    count += itttSteam.count;
+                    if (itttSteam.count != 0)
+                        sellItems.Add(itttSteam);
+                }
+            }
+            if (count == 0)
+            {
+                foreach (var itttSteam in tttSteam)
+                {
+                    count += itttSteam.count;
+                }
+            }
+            return sellItems;
+        }
+        public void AddParameters(DataGridView dataSellSteam)
+        {
+            for (int i = 0; i < dataSellSteam.Rows.Count; i++)
+            {
+                MarketItemInfo info = Info.GetMarketItemInfo(@"https://market.csgo.com/api/ItemInfo/" + dataSellSteam.Rows[i].Cells["classid"].Value + "_"
+                    + dataSellSteam.Rows[i].Cells["instanceid"].Value + "/ru/?key=" + keyMarket);
+                MarketItemHistory history = Info.GetMarketItemHistory(@"https://market.csgo.com/api/ItemHistory/" + info.classid + "_"
+                    + info.instanceid + "?key=" + keyMarket);
+                dataSellSteam.Rows[i].Cells["min_price"].Value = Convert.ToDouble(Math.Abs(info.min_price)) / 100;
+                dataSellSteam.Rows[i].Cells["name"].Value = info.name;
+                dataSellSteam.Rows[i].Cells["type"].Value = info.type;
+                dataSellSteam.Rows[i].Cells["average_market_price"].Value = Convert.ToDouble(history.average) / 100;
+                dataSellSteam.Rows[i].Cells["lowest_market_price"].Value = Convert.ToDouble(history.min) / 100;
+                dataSellSteam.Rows[i].Cells["highest_market_price"].Value = Convert.ToDouble(history.max) / 100;
+            }
+        }
+        public void SetDataSourceSellItem(DataGridView dataGridViewForSell)
+        {
+            marketItems = Converter.InventoryListToBindingList(Info.GetUrlJSONInventorylist(@"https://market.csgo.com/api/Trades/?key=" + keyMarket));
+            var targetListSteam = itemsrgInventory.Select(x =>
+                new ItemId() { classid = x["classid"].ToString(), instanceid = x["instanceid"].ToString() }).ToList();
+            var targetListMarket = marketItems.Select(x =>
+                new ItemId() { classid = x.i_classid, instanceid = x.ui_real_instance }).ToList();
+            int count = 0;
+            soldSteamItems = SoldItemsFromSteam(targetListSteam, targetListMarket, ref count);
+            dataGridViewForSell.DataSource = soldSteamItems;
+            labelCount.Content = "Count: " + count.ToString();
+            AddParameters(dataGridViewForSell);
+            FormsLogic.AddSteamPrices(ref dataGridViewForSell);
+            FormsLogic.AddEarnings(ref dataGridViewForSell);
+        }
+
+        public void SetDataSourceAllItem()
+        {
+            //lightnining Y9PRN
+            JObject result = null;
+            Info.GetUrlJSON(@"http://steamcommunity.com/profiles/76561198131830880/inventory/json/730/2/?trading=1", ref result);
+            itemsrgInventory = Converter.jObjectToList("rgInventory", result);
+            dataGridViewAllItems.ItemsSource = itemsrgInventory;
+        }
+        private void FormInventorySteam_Load(object sender, EventArgs e)
+        {
+            //dataGridView1.DataSource = SearchBot.GetUrlJSONInventorySteamlist(@"http://steamcommunity.com/profiles/76561198131830880/inventory/json/570/2/?trading=1");
+            SetDataSourceAllItem();
+            FormsLogic.AddColumns(ref dataGridViewForSell, new List<string>()
+            {
+                "name",
+                "type",
+                "min_price",
+                "average_market_price",
+                "lowest_market_price",
+                "highest_market_price",
+                "average_price",
+                "median_price",
+                "highest_price",
+                "lowest_price",
+                "min_earnings",
+                "average_earnings",
+                "max_earnings"
+            });
+            SetDataSourceSellItem(dataGridViewForSell);
+        }
+
+        private void SetDataSourceSellItem(System.Windows.Controls.DataGrid dataGridViewForSell)
+        {
+            marketItems = Converter.InventoryListToBindingList(Info.GetUrlJSONInventorylist(@"https://market.csgo.com/api/Trades/?key=" + keyMarket));
+            var targetListSteam = itemsrgInventory.Select(x =>
+                new ItemId() { classid = x["classid"].ToString(), instanceid = x["instanceid"].ToString() }).ToList();
+            var targetListMarket = marketItems.Select(x =>
+                new ItemId() { classid = x.i_classid, instanceid = x.ui_real_instance }).ToList();
+            int count = 0;
+            soldSteamItems = SoldItemsFromSteam(targetListSteam, targetListMarket, ref count);
+            dataGridViewForSell.ItemsSource = soldSteamItems;
+            labelCount.Content = "Count: " + count.ToString();
+            AddParameters(dataGridViewForSell);
+            FormsLogic.AddSteamPrices(ref dataGridViewForSell);
+            FormsLogic.AddEarnings(ref dataGridViewForSell);
+        }
+
+        private void AddParameters(System.Windows.Controls.DataGrid dataSellSteam)
+        {
+            for (int i = 0; i < dataSellSteam.Items.Count; i++)
+            {
+                MarketItemInfo info = Info.GetMarketItemInfo(@"https://market.csgo.com/api/ItemInfo/" +
+                    (dataSellSteam.Items[i] as ItemId).classid + "_"
+                    + (dataSellSteam.Items[i] as ItemId).instanceid 
+                    + "/ru/?key=" + keyMarket);
+                MarketItemHistory history = Info.GetMarketItemHistory(@"https://market.csgo.com/api/ItemHistory/" + info.classid + "_"
+                    + info.instanceid + "?key=" + keyMarket);
+
+                var newItem = new dataSellSteamItem
+                {
+                    min_price = "" + Convert.ToDouble(Math.Abs(info.min_price)) / 100,
+                    name = info.name,
+                    type = info.type,
+                    average_market_price = "" + Convert.ToDouble(history.average) / 100,
+                    lowest_market_price = "" + Convert.ToDouble(history.min) / 100,
+                    highest_market_price = "" + Convert.ToDouble(history.max) / 100,
+                };
+
+                FormsLogic.SaveItem(ref dataSellSteam, i, newItem);
+            }
+        }
+
+        public static void SaveItem(ref System.Windows.Controls.DataGrid dataSellSteam, int i, dataSellSteamItem newItem)
+        {
+            var items = dataSellSteam.Items;
+            items[i] = newItem;
+            dataSellSteam.ItemsSource = items;
+        }
+
+        public class dataSellSteamItem
+        {
+            public string min_price;
+            public string name;
+            public string type;
+            public string average_market_price;
+            public string lowest_market_price;
+            public string highest_market_price;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //rowIndex = e.RowIndex;
+            //instanceid = dataGridViewAllItems.Rows[rowIndex].Cells["instanceid"].Value.ToString();
+            //classid = dataGridViewAllItems.Rows[rowIndex].Cells["classid"].Value.ToString();
+        }
+
+        private void dataGridViewForSell_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1)
+            {
+                rowIndex = e.RowIndex;
+                instanceid = (dataGridViewForSell.Items[rowIndex] as ItemId).instanceid;
+                classid = (dataGridViewForSell.Items[rowIndex] as ItemId).classid;
+            }
+        }
+
+        public void SellItems(DataGridView items, int rowIndex, IProgress<string> progress)
+        {
+            for (int i = 0; i < Convert.ToInt32(items.Rows[rowIndex].Cells["count"].Value); i++)
+            {
+                Info.GetUrl(@"https://market.csgo.com/api/SetPrice/new_" + items.Rows[rowIndex].Cells["classid"].Value.ToString() + "_" +
+                    items.Rows[rowIndex].Cells["instanceid"].Value.ToString() + @"/" + textBoxPrice.Text + @"/?key=" + keyMarket).ToString();
+                Thread.Sleep(100);
+                progress.Report(Convert.ToDouble((i + 1) * 100 / Convert.ToInt32(items.Rows[rowIndex].Cells["count"].Value)).ToString() + "%");
+            }
+        }
+
+        private void dataGridViewForSell_ColumnHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (currentColumnIndex == e.ColumnIndex)
+            {
+                soldSteamItems = new BindingList<ItemId>(soldSteamItems.OrderByDescending(
+                    x => x.GetType().GetProperty(dataGridViewForSell.Columns[e.ColumnIndex].Header.ToString()).GetValue(x, null)
+                    ).ToList());
+                currentColumnIndex = 0;
+            }
+            else
+            {
+                soldSteamItems = new BindingList<ItemId>(soldSteamItems.OrderBy(
+                        x => x.GetType().GetProperty(dataGridViewForSell.Columns[e.ColumnIndex].Header.ToString()).GetValue(x, null)
+                        ).ToList());
+                currentColumnIndex = e.ColumnIndex;
+            }
+            dataGridViewForSell.ItemsSource = soldSteamItems;
+        }
+
+        private async void buttonSell_Click(object sender, EventArgs e)
+        {
+            var progress = new Progress<string>(s => labelProgress.Content = s);
+            await Task.Factory.StartNew(() =>
+            {
+                buttonSell.Dispatcher.BeginInvoke((MethodInvoker)(() =>
+                {
+                    buttonSell.IsEnabled = false;
+                }));
+                SellItems(dataGridViewForSell, rowIndex, progress);
+            });
+            SetDataSourceSellItem(dataGridViewForSell);
+            buttonSell.IsEnabled = true;
+        }
+
+        private void SellItems(System.Windows.Controls.DataGrid items, int rowIndex, IProgress<string> progress)
+        {
+            for (int i = 0; i < Convert.ToInt32((items.Items[rowIndex] as ItemId).count); i++)
+            {
+                Info.GetUrl(@"https://market.csgo.com/api/SetPrice/new_" + (items.Items[rowIndex] as ItemId).classid.ToString() + "_" +
+                    (items.Items[rowIndex] as ItemId).instanceid.ToString() + @"/" + textBoxPrice.Text + @"/?key=" + keyMarket).ToString();
+                Thread.Sleep(100);
+                progress.Report(Convert.ToDouble((i + 1) * 100 / Convert.ToInt32((items.Items[rowIndex] as ItemId).count)).ToString() + "%");
+            }
         }
     }
 }
